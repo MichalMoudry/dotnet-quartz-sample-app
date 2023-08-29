60fc68adcded4e0cfcb55aea01515b3fff200f6e
Michal Moudrý <moudry.michal@live.com>
2023-08-29 16:38:25 +0200

    Changes to app

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     engine = db_init.get_engine(True)
     db_init.create_tables(engine)
 
+    schedule.every(5).seconds.do(print, "Hello!")
     with engine.connect() as conn:
         while True:
             schedule.run_pending()
--- services/jobs.py
+++ services/jobs.py
@@ -2,6 +2,32 @@
 Module with background tasks.
 """
 
+from abc import ABC, abstractmethod
+
+class BaseJob(ABC):
+    """
+    A base class for job classes.
+    """
+    def __init__(self, job_name: str) -> None:
+        self._job_name = job_name
+    
+    @property
+    @abstractmethod
+    def job(self) -> (...):
+        """
+        A property containing code that will be executed.
+        """
+        pass
+
+
+class HelloJob(BaseJob):
+    """
+    A class for a basic job that prints 'Hello!'.
+    """
+    def __init__(self, job_name: str) -> None:
+        super().__init__(job_name)
+
+
 def hello_job():
     """
     A simple hello job that prints 'Hello!'.
